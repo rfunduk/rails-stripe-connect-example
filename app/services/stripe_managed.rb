a08d9ae5faadaf1d3f179942dae8ea94b7b07ff7
Andrew Monks <a@monks.co>
2015-04-13 03:57:33 -0500

    fix typo (managed_status to account_status)
    use `account_status` instead of undefined `managed_status`
    
    Perhaps you'd rather change line 114 to `def managed_status`; I figured I'd err towards a consistent interface, but idk if you made it different for a particular reason.

--- app/services/stripe_managed.rb
+++ app/services/stripe_managed.rb
@@ -40,7 +40,7 @@ class StripeManaged < Struct.new( :user )
         stripe_user_id: @account.id,
         secret_key: @account.keys.secret,
         publishable_key: @account.keys.publishable,
-        stripe_account_status: managed_status
+        stripe_account_status: account_status
       )
     end
 
